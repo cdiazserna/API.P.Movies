c2e0b0a864bb359d84fbaf31897dbf392246242a
cjuni <cjuni@JRSOFTWAREDEV>
2025-10-24 19:33:05 -0500

    Add project files.

---
+++ API.P.Movies/DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.P.Movies.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.P.Movies.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Todos los DBSets - las nuevas tablas
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
